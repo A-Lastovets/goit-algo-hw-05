58fc26d9a2182f7d0665186d608c8a93873bb30a
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-05-10 17:16:14 +0200

    task4_changed

--- task4_main.py
+++ task4_main.py
@@ -1,13 +1,59 @@
+#decorator
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            return f"ValueError: {str(e)}\nPlease provide correct arguments."
+        except KeyError as k:
+            return f"KeyError: {str(k)}\nEnter a valid command again\n"
+        except IndexError as i:
+            return f"IndexError: {str(i)} Invalid number of arguments."
+        
+    return inner
 
-from task4_add import add_contact, change_contact
-from task4_show import show_phone, show_all
-from decorators import input_error1
 
-@input_error1
+@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
+
+
+@input_error
+def add_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError("Give me name and phone please, try again.")
+    name, phone = args
+    if name in contacts:
+        raise KeyError(f"User '{name}'  is already in the contacts: {contacts}")
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError("Give me name and phone please, try again.")
+    name, phone = args
+    if name not in contacts:
+        raise KeyError(f"User '{name}' is not found in contacts: {contacts}")
+    contacts[name] = phone
+    return "Contact updated."
+
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    if name in contacts:
+        print(contacts[name])
+    else:
+        print(f"User '{name}' is not found in contacts: {contacts}")
+
+
+@input_error
+def show_all(contacts):
+
+    for info in contacts:
+        print(f"User '{info}', phone number '{contacts[info]}'")
 
 
 def main():
@@ -26,8 +72,8 @@ def main():
             case "hello":    print("How can I help you?")
             case "add":      print(add_contact(args, contacts))
             case "change":   print(change_contact(args, contacts))
-            case "phone":    show_phone(args, contacts)
-            case "all" if len(args) < 1:  show_all(contacts)       # замість декоратора перевірка відбувається тут
+            case "phone" if len(args) == 1:   show_phone(args, contacts)
+            case "all"   if len(args) < 1:    show_all(contacts)    
             case _:          print("Invalid command, please try again. \nIf you want to exit the bot : type 'exit' or 'close'\n ")
 
 if __name__ == "__main__":
